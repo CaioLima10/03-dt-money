26f90c7fd1c4dfc2d994e696be108f5cb7a74f72
CaioLima10 <caiohenrys50@gmail.com>
2025-01-18 21:51:12 -0300

    reduce income, outcome and total

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,28 @@
+import { useContext } from "react";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
 import { MdAttachMoney } from "react-icons/md";
+import { TransactionsContext } from "../../context/transactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -11,7 +31,7 @@ export function Summary() {
           <FaRegArrowAltCircleUp size={32} color="#00875F" />
         </header>
 
-        <strong>R$ 1.125,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -19,7 +39,7 @@ export function Summary() {
           <FaRegArrowAltCircleDown size={32} color="#AB322E" />
         </header>
 
-        <strong>R$ 1.125,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
@@ -27,7 +47,7 @@ export function Summary() {
           <MdAttachMoney size={32} color="#E1E1E6" />
         </header>
 
-        <strong>R$ 1.125,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
